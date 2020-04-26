77949a8592b05307e5caf55dd2fbe9b4b4806075
PrashanthaRK <52959505+PrashanthaRK@users.noreply.github.com>
2020-04-27 01:22:53 +0530

    Create Common.java

---
+++ src/com/squareshift/Common.java
@@ -0,0 +1,9 @@
+package com.squareshift;
+
+public class Common {
+	public static final int NOT_OCCUPIED = 0;
+	public static final int AISLE_SEAT = 1;
+	public static final int WINDOW_SEAT = 2;
+	public static final int MIDDLE_SEAT = 3;
+	
+}
