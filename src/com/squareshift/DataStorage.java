99dccfbda19525e12728ed0fdd87c1f29f8cc01e
PrashanthaRK <52959505+PrashanthaRK@users.noreply.github.com>
2020-04-27 01:19:36 +0530

    Update DataStorage.java

--- src/com/squareshift/DataStorage.java
+++ src/com/squareshift/DataStorage.java
@@ -5,9 +5,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 class DataStorage {
-	static Map<Integer, Integer> rowColmapper;	
+	//static Map<Integer, Integer> rowColmapper;
+	static ArrayList<Map> mapList;
 	static Map<Integer, Integer> seatPosMapper;
 	static int noOfSeats, totalNoOfSeats;
+	static ArrayList<Integer> seatsMapSequence;
 	static ArrayList<Integer> seatsOccupiedSequence;
 	private static DataStorage dataStorage= new DataStorage();
 	public DataStorage(){		
@@ -17,8 +19,8 @@ class DataStorage {
 			dataStorage = new DataStorage();
 		return dataStorage;
 	}
-	public static void setRowColMap(HashMap map){
-		rowColmapper = (Map)map;
+	public static void setRowColMapList(ArrayList<Map> list){
+		mapList = list;
 	}
 	public static void setNoOfSeats(int seats){
 		noOfSeats = seats;
@@ -26,8 +28,8 @@ class DataStorage {
 	public static int getNoOfSeats(){
 		return noOfSeats;		
 	}
-	public static Map getRowColMap(){
-		return rowColmapper;
+	public static ArrayList<Map> getRowColMapList(){
+		return mapList;
 	}
 	public static void setTotalNoOfSeats(int seats){
 		totalNoOfSeats = seats;
@@ -37,7 +39,7 @@ class DataStorage {
 	}
 
 	public static int getSeatsOccupiedSequence(int seatNo){
-		return seatsOccupiedSequence.get(seatNo);	
+		return seatsOccupiedSequence.get(seatNo).intValue();	
 	}
 	
 	public static Map getSeatPosMapper(){
@@ -50,7 +52,24 @@ class DataStorage {
 	public void setSeatsOccupiedSequence( ArrayList<Integer> array){
 		seatsOccupiedSequence = array;
 	}
+
+	public void setSeatsOccupiedSequence(int position, int value){
+		seatsOccupiedSequence.set(position, value);
+	}
+
 	public ArrayList<Integer> getSeatsOccupiedSequence(){
 		return seatsOccupiedSequence;
+	}
+	
+	
+	public ArrayList<Integer> getSeatsMapSequence(){
+		return seatsMapSequence;
+	}
+	public static int getSeatsMapdSequence(int seatNo){
+		return seatsMapSequence.get(seatNo).intValue();	
+	}
+	
+	public void setSeatsMapSequence( ArrayList<Integer> array){
+		seatsMapSequence = array;
 	}
 }
