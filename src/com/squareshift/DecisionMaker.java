7dc175a29c6a74edb17b2a24cbb956a1a6ff8cfe
PrashanthaRK <52959505+PrashanthaRK@users.noreply.github.com>
2020-04-27 01:20:33 +0530

    Update DecisionMaker.java

--- src/com/squareshift/DecisionMaker.java
+++ src/com/squareshift/DecisionMaker.java
@@ -8,47 +8,53 @@ import java.util.Set;
 
 public class DecisionMaker {
 	boolean isAsileSeat(int seatNo){
-		Integer seatMap = (Integer)DataStorage.getInstance().getSeatsOccupiedSequence().get(seatNo);
-		if(seatMap.intValue() == 1)
+		Integer seatMap = (Integer)DataStorage.getInstance().getSeatsMapSequence().get(seatNo);
+		if(seatMap.intValue() == Common.AISLE_SEAT)
 			return true;
 		return false;
 	}
 	
 	boolean isWindowsSeat(int seatNo){
-		Integer seatMap = (Integer)DataStorage.getInstance().getSeatsOccupiedSequence().get(seatNo);
-		if(seatMap.intValue() == 2)
+		Integer seatMap = (Integer)DataStorage.getInstance().getSeatsMapSequence().get(seatNo);
+		if(seatMap.intValue() == Common.WINDOW_SEAT)
 			return true;
 		return false;	
 	}
 	
 	boolean isMiddleSeat(int seatNo){
-		Integer seatMap = (Integer)DataStorage.getInstance().getSeatsOccupiedSequence().get(seatNo);
-		if(seatMap.intValue() == 1 || seatMap.intValue() == 2 )
+		Integer seatMap = (Integer)DataStorage.getInstance().getSeatsMapSequence().get(seatNo);
+		if(seatMap.intValue() == Common.AISLE_SEAT || seatMap.intValue() == Common.WINDOW_SEAT )
 			return false;
 		return true;
 	}
 	
 	boolean isSetOccupied(int i){
-		if (DataStorage.getSeatsOccupiedSequence(i) == -1)
+		if (DataStorage.getSeatsOccupiedSequence(i) != Common.NOT_OCCUPIED)
 			return true;
 		return false;
 	}
 	
 	int getNextSeatNo(int currentSeatNo){
-		for(int i = 0; i < DataStorage.getInstance().getNoOfSeats(); i++){
+		for(int i = 0; i < DataStorage.getInstance().getTotalNoOfSeats(); i++){
 			if(!isSetOccupied(i)){				
-				if(isAsileSeat (i))
+				if(isAsileSeat (i)){
+					//ArrayList<Integer> array = DataStorage.getInstance().getSeatsOccupiedSequence();
+					DataStorage.getInstance().setSeatsOccupiedSequence(i, Common.AISLE_SEAT);
 					return i;
+				}
 			}
 		}
-		for(int i = 0; i < DataStorage.getInstance().getNoOfSeats(); i++){
+		for(int i = 0; i < DataStorage.getInstance().getTotalNoOfSeats(); i++){
 			if(!isSetOccupied(i)){	
-				if(isWindowsSeat (i))
+				if(isWindowsSeat (i)){
+					DataStorage.getInstance().setSeatsOccupiedSequence(i, Common.WINDOW_SEAT);
 					return i;
+				}
 			}
 		}
-		for(int i = 0; i < DataStorage.getInstance().getNoOfSeats(); i++){
+		for(int i = 0; i < DataStorage.getInstance().getTotalNoOfSeats(); i++){
 			if(!isSetOccupied(i)){
+				DataStorage.getInstance().setSeatsOccupiedSequence(i, Common.MIDDLE_SEAT);
 				return i;
 			}			
 		}
@@ -56,11 +62,11 @@ public class DecisionMaker {
 	}
 	
 	int getTotalNoOfSeats(){
-		Map<Integer, Integer> map = DataStorage.getInstance().getRowColMap();
+		ArrayList<Map> mapList = DataStorage.getInstance().getRowColMapList();
 		int count = 0;
-		Set set = map.keySet();
-		Iterator iterator = set.iterator();
-		while(iterator.hasNext()) {
+		for (Map map: mapList){
+			Set set = map.keySet();
+			Iterator iterator = set.iterator();
 			Integer row = (Integer)iterator.next();
 			Integer col = (Integer)map.get(row);
 			count = count + row.intValue() * col.intValue();
